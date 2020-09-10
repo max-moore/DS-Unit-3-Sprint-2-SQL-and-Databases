8861194bc9f03b43856b7298f22437ec4878f2b4
max-moore <maxwell.moorebillings@gmail.com>
2020-09-09 22:01:24 -0700

    Finalized Tuesday lecture work

--- module2-sql-for-analysis/elephant_queries.py
+++ module2-sql-for-analysis/elephant_queries.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import psycopg2
+from psycopg2.extras import execute_values
+import json
 
 load_dotenv()
 
@@ -19,19 +21,29 @@ cursor.execute('SELECT * from test_table;')
 result = cursor.fetchall()
 print(result)
 
-insertion_sql="""
-INSERT INTO test_table (name, data) VALUES
-(
-  'A row name',
-  null
-),
-(
-  'Another row, with JSON',
-  '{ "a": 1, "b": ["dog", "cat", 42], "c": true }'::JSONB
-);
-"""
+# insertion_sql="""
+# INSERT INTO test_table (name, data) VALUES
+# ('A row name', null),
+# ('Another row, with JSON', '{ "a": 1, "b": ["dog", "cat", 42], "c": true }'::JSONB);
+# """
+# cursor.execute(insertion_sql)
 
-cursor.execute(insertion_sql)
+my_dict = { "a": 1, "b": ["dog", "cat", 42], "c": 'true' }
+insertion_query = "INSERT INTO test_table (name, data) VALUES %s"
+
+# cursor.execute(insertion_query,
+#     ('A rowwwww', 'null')
+# )
+
+# cursor.execute(insertion_query,
+#     ('Another row, with JSONNNNN', json.dumps(my_dict))
+# )
+
+execute_values(cursor, insertion_query, [
+    ('A rowwwww', 'null'),
+    ('Another row, with JSONNNNN', json.dumps(my_dict)),
+    ('Third row', "3")
+])
 
 connection.commit()
 
